2f3cc6a77c3f120bc6520a8176a45c3264ad6eb9
ttttgeorge <takayuki.hosokawa@gmail.com>
2021-06-28 15:46:26 +0900

    1st commit

---
+++ Desktop/sample_streamlit/main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from PIL import Image
+from PIL import ImageDraw
+
+st.title('写真表示テスト')
+uploaded_file = st.file_uploader('Choose an image…', type='jpg')
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    draw = ImageDraw(img)
+    draw.line([(0, 50), (200, 50), (0, 150), (200, 150)], fill='red', width=5)
+    st.image(img, caption='Uploaded Image', use_column_width=True)
+
+
